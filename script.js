0cbeee8f03592e00d9eafb5a02ee0a5394f0fdbe
the-obonnet <60609272+the-obonnet@users.noreply.github.com>
2020-02-03 15:14:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function toggleAbstractLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "Abstract-mode" ? "light-mode" : "Abstract-mode";
+}
+
+function toggleIntroLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "Intro-mode" ? "light-mode" : "Intro-mode";
+}
+
+function togglePartie1Light() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "Partie1-mode" ? "light-mode" : "Partie1-mode";
+}
+
+function togglePartie2Light() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "Partie2-mode" ? "light-mode" : "Partie2-mode";
+}
+
+function togglePartie3Light() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "Partie3-mode" ? "light-mode" : "Partie3-mode";
+}
+
+function togglePartie4Light() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "Partie4-mode" ? "light-mode" : "Partie4-mode";
+}
+
+function toggleConcluLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "Conclu-mode" ? "light-mode" : "Conclu-mode";
+}
+
+function toggleSommaireLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "Sommaire-mode" ? "light-mode" : "Sommaire-mode";
+}
